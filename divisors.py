4cbc5228d924e1b46163167a5858b18816420f7f
DDoyoun <162810196+DDoyoun@users.noreply.github.com>
2024-11-09 15:25:27 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -2,8 +2,8 @@ import sys
 
 number = int(sys.argv[1])
 
-for i in range(1, number):
-  if 100 % i == 0:
-    print(i, end=' ')
+for i in range(1, number): # loop between 1 and number
+  if 100 % i == 0:# check if remainder is 0
+    print(i, end=" ")
 
 print()
